1229c536859a7f06f8499fe8990f048dd26e0d4f
ryik5 <ryik.yuri@gmail.com>
2020-06-19 17:15:09 +0300

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GuiPackager
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void Packager()
+        {
+
+            var fileInputName = @"OneExeProgram\Libraries\AutoMapper 1.0 RTW\AutoMapper.dll";
+            var assembly = File.ReadAllBytes(fileInputName);
+
+            var fileOutputName = @"OneExeProgram\Libraries\AutoMapper 1.0 RTW\AutoMapper.dll.deflated";
+            using (var file = File.Open(fileOutputName, FileMode.Create))
+            using (var stream = new DeflateStream(file, CompressionMode.Compress))
+            using (var writer = new BinaryWriter(stream))
+            {
+                writer.Write(assembly);
+            }
+        }
+    }
+}
