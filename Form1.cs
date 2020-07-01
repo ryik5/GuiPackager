e9f2db0d6aed2e0568cac5b6412d803f2fcf2ca0
ryik5 <ryik.yuri@gmail.com>
2020-07-01 16:56:30 +0300

    added readme

--- Form1.cs
+++ Form1.cs
@@ -1,13 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
 using System.IO;
 using System.IO.Compression;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace GuiPackager
@@ -57,10 +50,9 @@ namespace GuiPackager
            AssemblyLoader.RegisterAssemblyLoader();
        }
 
-
-
 using System;
 using System.IO;
+using System.IO.Compression;
 using System.Linq;
 using System.Reflection;
 
@@ -68,53 +60,128 @@ internal static class AssemblyLoader
 {
        internal static void RegisterAssemblyLoader()
        {
-           AppDomain currentDomain = AppDomain.CurrentDomain;
-           currentDomain.AssemblyResolve -= OnResolveAssembly;
-           currentDomain.AssemblyResolve += OnResolveAssembly;
-       }
+          //https://stackoverrun.com/ru/q/4794740
+            AppDomain currentDomain = AppDomain.CurrentDomain;
+            currentDomain.AssemblyResolve -= OnResolveAssembly;
+            currentDomain.AssemblyResolve += OnResolveAssembly;
+            WriteAppropriateVersionSqliteIteropLocaly();
+        }
 
-       private static Assembly OnResolveAssembly(object sender, ResolveEventArgs args)
-       {
-           return LoadAssemblyFromManifest(args.Name);
-       }
+        private static Assembly OnResolveAssembly(object sender, ResolveEventArgs args)
+        {
+            return LoadAssemblyFromManifest(args.Name);
+        }
 
-       private static Assembly LoadAssemblyFromManifest(string targetAssemblyName)
-       {
-           Assembly executingAssembly = Assembly.GetExecutingAssembly();
-           AssemblyName assemblyName = new AssemblyName(targetAssemblyName);
+        private static void WriteAppropriateVersionSqliteIteropLocaly()
+        {
+            var interopFileName = "SQLite.Interop.dll";
+            Assembly executingAssembly = Assembly.GetExecutingAssembly();
 
-           string resourceName = DetermineEmbeddedResourceName(assemblyName, executingAssembly);
+            var env = Environment.Is64BitProcess ? "x64" : "x86";// You might need to adjust this line to correctly specify 
+                                                                 // the embedded resource path within the custom action assembly.
 
-           using (Stream stream = executingAssembly.GetManifestResourceStream(resourceName))
-           {
-               if (stream == null)
-                   return null;
+            var resourceName = $"{executingAssembly.GetName().Name}.Resources.{env}.{interopFileName}.deflated";
 
-               byte[] assemblyRawBytes = new byte[stream.Length];
-               stream.Read(assemblyRawBytes, 0, assemblyRawBytes.Length);
+            CommonExtensions.Logger(LogTypes.Trace, "Try to write dll: " + resourceName);
 
+            try
+            {
+                var assemblyDirectory = Path.GetDirectoryName(executingAssembly.Location);
+                var dir = Directory.CreateDirectory($@"{assemblyDirectory}\{env}");
+                var interopFilePath = Path.Combine(dir.FullName, interopFileName);
+                FileInfo fi = new FileInfo(interopFilePath);
+                if (!File.Exists(interopFilePath) || fi.Length < 100)
+                    using (Stream stream = executingAssembly.GetManifestResourceStream(resourceName))
+                    {
+                        if (stream == null)
+                        {
+                            return;
+                        }
 
-               return Assembly.Load(assemblyRawBytes);
-           }
+                        using (var deflated = new DeflateStream(stream, CompressionMode.Decompress))
+                        using (var fs = new FileStream(interopFilePath, FileMode.Create, FileAccess.Write))
+                        {
+                            deflated.CopyTo(fs);
+                        }
+                    }
+            }
+            catch (Exception err)
+            {
+                CommonExtensions.Logger(LogTypes.Error, "Error writing: " + err.ToString());
+            }
+        }
 
-       }
+        private static Assembly LoadAssemblyFromManifest(string targetAssemblyName)
+        {
+            Assembly executingAssembly = Assembly.GetExecutingAssembly();
+            byte[] assemblyRawBytes = null;
 
-       private static string DetermineEmbeddedResourceName(AssemblyName assemblyName, Assembly executingAssembly)
-       {
-           //This assumes you have the assemblies in a folder named "EmbeddedAssemblies"
-           string resourceName = string.Format("{0}.EmbeddedAssemblies.{1}.dll",
-                                               executingAssembly.GetName().Name, assemblyName.Name);
+            //var names = typeof(Program).Assembly.GetManifestResourceNames();
+            //foreach (var n in names)
+            //{
+            //    CommonExtensions.Logger(LogTypes.Info, "names: " + n);
+            //}
 
-           //This logic finds the assembly manifest name even if it's not an case match for the requested assembly                          
-           var matchingResource = executingAssembly.GetManifestResourceNames()
-                                                   .FirstOrDefault(res => res.ToLower() == resourceName.ToLower());
+            try
+            {
+                CommonExtensions.Logger(LogTypes.Info, $"OnResolveAssembly, targetAssemblyName: {targetAssemblyName}");
+                AssemblyName assemblyName = new AssemblyName(targetAssemblyName);
 
-           if (matchingResource != null)
-           {
-               resourceName = matchingResource;
-           }
-           return resourceName;
-       }
+                string resourceName = DetermineEmbeddedResourceName(assemblyName, executingAssembly);
+                CommonExtensions.Logger(LogTypes.Info, $"OnResolveAssembly, resourceName: {resourceName}");
+
+                using (Stream stream = executingAssembly.GetManifestResourceStream(resourceName))
+                {
+                    if (stream == null)
+                    {
+                        return null;
+                    }
+
+                    using (var deflated = new DeflateStream(stream, CompressionMode.Decompress))
+                    using (var reader = new BinaryReader(deflated))
+                    {
+                        var ten_megabytes =10* 1024 * 1024;
+                        assemblyRawBytes = reader.ReadBytes(ten_megabytes);
+                    }
+                }
+            }
+            catch(Exception err)
+            {
+                CommonExtensions.Logger(LogTypes.Error, $"err: {err.ToString()}");
+                CommonExtensions.Logger(LogTypes.Error, $"error  -/-/-/-/   OnResolveAssembly, targetAssemblyName: {targetAssemblyName}");
+            }
+
+            return Assembly.Load(assemblyRawBytes);
+        }
+
+        private static string DetermineEmbeddedResourceName(AssemblyName assemblyName, Assembly executingAssembly)
+        {
+            //This assumes you have the assemblies in a folder named "Resources"
+            //in ahead all needed library files *.dll make as deflated files by 'GuiPackager'
+            //then add them into this previously created project folder - 'Resources'
+            //after it
+            //for every deflated files set a flag 'Build Action' in Property(Solution Explorer) as -  'Embedded Resource'
+            //then change for matched every library dll in 'Preferences' a flag 'Copy Local' as 'False'
+            var env = Environment.Is64BitProcess ? "x64" : "x86";
+            CommonExtensions.Logger( LogTypes.Info,$"DetermineEmbeddedResourceName, 1: {executingAssembly.GetName().Name}|2: {assemblyName.Name}");
+           string resourceName = $"{executingAssembly.GetName().Name}.Resources.{assemblyName.Name}.dll.deflated";
+            
+            if (assemblyName.Name.ToLower().Contains("sqlite.interop"))
+            {
+                resourceName = $"{executingAssembly.GetName().Name}.Resources.{env}.{assemblyName.Name}.dll.deflated";
+            }
+
+            //This logic finds the assembly manifest name even if it's not an case match for the requested assembly                          
+            var matchingResource = executingAssembly
+                .GetManifestResourceNames()
+                .FirstOrDefault(res => res.ToLower() == resourceName.ToLower());
+
+            if (matchingResource != null)
+            {
+                resourceName = matchingResource;
+            }
+            return resourceName;
+        }
 }
 
         */
---
+++ README.md
@@ -0,0 +1,23 @@
+### GUI Packager
+Making of packed(Deflate) files of project's libraries for embedding into the assebly .NET
+
+It is maintained by [@ryik5](https://github.com/ryik5).
+
+
+## What?
+
+This application makes packed(Deflate) files those could be used as embedded resources in asseblies.
+
+## Quickstart
+
+### Usage
+
+1. Run GuiPackager.exe
+2. Select file_for_deflate.dll
+3. Deflate selected
+
+# Links
+* [What?](https://github.com/ryik5/GuiPackager/README.md)
+* [Usage](https://github.com/ryik5/GuiPackager/README.md)
+* [Documentation](https://github.com/ryik5/GuiPackager/README.md)
+* [Contributing](https://github.com/ryik5/GuiPackager/README.md)
